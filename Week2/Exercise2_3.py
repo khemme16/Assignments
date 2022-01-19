b00dc0d9d5b13c064f952a5b34b994f6c009fab5
Kimberly Hemme <hemmek@student.ncmich.edu>
2022-01-18 19:55:43 -0500

    Initial commit

---
+++ Week2/Exercise2_3.py
@@ -0,0 +1,4 @@
+hours = int (input ("enter hours;"))
+rate = int (input ("enter rate;"))
+pay = hours * rate
+print ("pay", pay)
---
+++ Week2/Exercise2_5.py
@@ -0,0 +1,3 @@
+tempC = int (input ("how hot is it in celsius"))
+tempF = tempC * (9/5) + 32
+print ("it is", tempF, "in fahrenheit")
